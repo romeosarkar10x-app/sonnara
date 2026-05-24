4a17eaf37d21107798526e13d7c9aaf2e3422760
Romeo Sarkar <romeosarkar10x@gmail.com>
2026-05-24 12:22:09 +0530

    Updated 'root' element logic

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
+const rootElement = document.createElement("div");
+rootElement.id = "root";
+
+createRoot(rootElement).render(
     <StrictMode>
         <App />
     </StrictMode>,
 );
+
+document.body.appendChild(rootElement);
